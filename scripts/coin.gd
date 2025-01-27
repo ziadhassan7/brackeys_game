21c93c08fad60800efcf3eaba47a8b3537a08001
Ziad H. <ziadhassan7@gmail.com>
2025-01-27 22:26:15 +0200

    FEAT(Audio): add music and sound effects

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup_animation")
