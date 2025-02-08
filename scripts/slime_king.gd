77bd305732ed3f5cdcd7450990f107bc3318ade2
Ziad H. <ziadhassan7@gmail.com>
2025-02-08 21:09:56 +0200

    FIX(Adjustments To Enemies)

--- scripts/slime_king.gd
+++ scripts/slime_king.gd
@@ -1,23 +1,26 @@
-extends Node2D
+extends Area2D
 
-const MAX_HEALTH = 100
+
+const MAX_HEALTH = 500
 
 var speed = 60
 var direction = 1 # 1: right , -1: left
 var health = MAX_HEALTH
 var is_dead = false
 
+@onready var slime_king: Area2D = $"."
+@onready var killzone: Area2D = $Killzone
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var death_sound: AudioStreamPlayer2D = $HurtSound
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var killzone: Area2D = $Killzone
-@onready var slime: Area2D = $"."
-@onready var death_sound: AudioStreamPlayer2D = $HurtSound
+
 
 
 # assign signal to detect if enemy is being hit (by using the built in area_entered)
 func _ready():
-	slime.connect("area_entered", Callable(self, "_on_hit"))
+	slime_king.connect("area_entered", Callable(self, "_on_hit"))
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -35,8 +38,7 @@ func _process(delta: float) -> void:
 		killzone.scale.x = 1
 		
 	
-	position.x += direction*(speed * delta) #we multiply by delta because the time between frames varies and we do not want the speed to be fluctuating, so we use delta to stablize the speed by overcompensating  the difference between each frame.
-	
+	position.x -= direction*(speed * delta) 
 
 
 # When enemy is hit by the player
@@ -69,11 +71,10 @@ func take_damage(damage: int):
 			killzone.set_deferred("visible", true)
 
 
-
 func die():
 	if is_dead:
 		return
-		
+
 
 	speed = 5  # Stop movement
 	animated_sprite_2d.play("death")  # Play death animation
@@ -82,5 +83,4 @@ func die():
 	await animated_sprite_2d.animation_finished
 	queue_free()  # Remove enemy after animation 
 	
-	GameManager.open_portal_to_boss_arena()
 	is_dead = true
